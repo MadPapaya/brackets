44131452ada32d353ee4d93648a8a7c174a3bdda
MadPapaya <weekofkex@gmail.com>
2021-03-05 18:33:26 +0300

    init: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketsMap = bracketsConfig.map((c) => c.join(""));
+
+    function recursion() {
+        for (const brackets of bracketsMap) {
+            if (str.length == 0) {
+                return true
+            }
+            if (str.includes(brackets)) {
+                str = str.replace(brackets, "");
+                return recursion();
+            }
+        }
+        return false;
+    }
+    return recursion();
 }
